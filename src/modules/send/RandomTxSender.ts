fc9b2a2f12ef2438b19f27f13a023b5564dc1133
Michael Kim <michael.kim.200518@gmail.com>
2021-07-30 17:45:01 +0900

    Update

--- src/modules/send/RandomTxSender.ts
+++ src/modules/send/RandomTxSender.ts
@@ -16,7 +16,7 @@ export class RandomTxSender {
             this.config.server.agora_endpoint.toString()
         );
     }
-
+    /*
     createTransaction(height: sdk.JSBI): Promise<sdk.Transaction> {
         return new Promise<sdk.Transaction>(async (resolve, reject) => {
             try {
@@ -120,6 +120,102 @@ export class RandomTxSender {
                         status: true,
                         data: h,
                     });
+                } else {
+                    return resolve({
+                        status: false,
+                        error: "The block height is 0",
+                    });
+                }
+            } catch (e) {
+                return resolve({
+                    status: false,
+                    error: e,
+                });
+            }
+        });
+    }
+
+    send(): Promise<any> {
+        return new Promise<any>(async (resolve) => {
+            await prepare();
+            WK.make();
+            let res = await this.makeBlock();
+            resolve(res);
+        });
+    }
+*/
+    public makeBlock(): Promise<any> {
+        return new Promise<any>(async (resolve, reject) => {
+            try {
+                let height: sdk.JSBI = sdk.JSBI.BigInt(0);
+
+                try {
+                    height = await this.boa_client.getBlockHeight();
+                } catch (e) {
+                    logger.error(e);
+                    return resolve({
+                        status: false,
+                        error: e,
+                    });
+                }
+
+                if (sdk.JSBI.greaterThan(height, sdk.JSBI.BigInt(0))) {
+                    let res: sdk.IWalletResult;
+                    try {
+                        let key_count = this.config.process.key_count;
+                        let sources: Array<number> = [];
+                        let source = Math.floor(Math.random() * key_count);
+                        while (sources.find((value) => value == source) !== undefined)
+                            source = Math.floor(Math.random() * key_count);
+                        sources.push(source);
+                        let source_key_pair = WK.keys(source);
+
+                        let destination = Math.floor(Math.random() * key_count);
+                        while (source === destination) destination = Math.floor(Math.random() * key_count);
+                        let destination_key_pair = WK.keys(destination);
+
+                        let wallet = new sdk.Wallet(source_key_pair, {
+                            agoraEndpoint: this.config.server.agora_endpoint.toString(),
+                            stoaEndpoint: this.config.server.stoa_endpoint.toString(),
+                            fee: sdk.WalletFeeOption.Medium,
+                        });
+                        let bal_res = await wallet.getBalance();
+                        if (bal_res.code !== sdk.WalletResultCode.Success || bal_res.balance === undefined) {
+                            return resolve({
+                                status: false,
+                                error: bal_res.message,
+                            });
+                        }
+                        let range = sdk.JSBI.BigInt(Math.floor(Math.random() * 50) + 20);
+                        let send_amount = sdk.JSBI.divide(
+                            sdk.JSBI.multiply(bal_res.balance.spendable, range),
+                            sdk.JSBI.BigInt(100)
+                        );
+                        res = await wallet.transfer([
+                            {
+                                address: destination_key_pair.address,
+                                amount: send_amount,
+                            },
+                        ]);
+                        if (res.code === sdk.WalletResultCode.Success) {
+                            let h = sdk.hashFull(res.tx).toString();
+                            logger.info(`TX_HASH (send / ${height.toString()}) : ${h}`);
+                            return resolve({
+                                status: true,
+                                data: h,
+                            });
+                        } else {
+                            return resolve({
+                                status: false,
+                                error: res.message,
+                            });
+                        }
+                    } catch (e) {
+                        return resolve({
+                            status: false,
+                            error: e,
+                        });
+                    }
                 } else {
                     return resolve({
                         status: false,
