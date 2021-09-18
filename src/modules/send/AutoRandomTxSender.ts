5613f2fb3495b486676829f0476f7198d00bdd3a
Michael Kim <michael.kim.200518@gmail.com>
2021-09-18 11:45:24 +0900

    update AutoRandomSender

--- src/modules/send/AutoRandomTxSender.ts
+++ src/modules/send/AutoRandomTxSender.ts
@@ -1,13 +1,12 @@
 import * as sdk from "boa-sdk-ts";
 
 import { Config } from "../common/Config";
-import { prepare } from "../utils/Process";
-import { WK } from "../utils/WK";
 import { logger } from "../common/Logger";
-import { IWalletResult } from "boa-sdk-ts";
-import { Balance } from "boa-sdk-ts/src/modules/net/response/Balance";
+import { prepare, wait } from "../utils/Process";
+import { WK } from "../utils/WK";
 
 export class AutoRandomTxSender {
+    private static doing_genesis: boolean = false;
     private boa_client: sdk.BOAClient;
     private config: Config;
 
@@ -21,22 +20,22 @@ export class AutoRandomTxSender {
 
     public isDistributed(): Promise<boolean> {
         return new Promise<any>(async (resolve, reject) => {
-            const wallet = new sdk.Wallet(WK.CommonsBudget(), {
+            const wallet = new sdk.Wallet(WK.Genesis(), {
                 agoraEndpoint: this.config.server.agora_endpoint.toString(),
                 stoaEndpoint: this.config.server.stoa_endpoint.toString(),
                 fee: sdk.WalletFeeOption.Medium,
             });
-            wallet.getBalance().then((res: IWalletResult) => {
+            wallet.getBalance().then((res: sdk.IWalletResult) => {
                 if (res.code !== sdk.WalletResultCode.Success || res.data === undefined) return reject();
 
-                let balance: Balance = res.data;
-                if (sdk.JSBI.equal(balance.balance, sdk.JSBI.BigInt(0))) return resolve(true);
-                else return resolve(false);
+                const balance: sdk.Balance = res.data;
+                if (sdk.JSBI.equal(balance.spendable, sdk.BOA(488_000_000).value)) return resolve(false);
+                else return resolve(true);
             });
         });
     }
 
-    public makeBlock(): Promise<any> {
+    public sendRandTransaction(): Promise<any> {
         return new Promise<any>(async (resolve, reject) => {
             const key_count = this.config.process.key_count;
             const source = Math.floor(Math.random() * key_count);
@@ -54,7 +53,7 @@ export class AutoRandomTxSender {
 
             wallet
                 .getBalance()
-                .then((bal_res: IWalletResult) => {
+                .then((bal_res: sdk.IWalletResult) => {
                     if (bal_res.code !== sdk.WalletResultCode.Success || bal_res.data === undefined) {
                         return resolve({
                             status: false,
@@ -82,7 +81,7 @@ export class AutoRandomTxSender {
                         .transfer(receivers)
                         .then((res) => {
                             if (res.code === sdk.WalletResultCode.Success) {
-                                let h = sdk.hashFull(res.data).toString();
+                                const h = sdk.hashFull(res.data).toString();
                                 return resolve({
                                     status: true,
                                     data: h,
@@ -110,20 +109,234 @@ export class AutoRandomTxSender {
         });
     }
 
-    send(): Promise<any> {
+    public createTransaction(): Promise<sdk.Transaction[]> {
+        return new Promise<sdk.Transaction[]>(async (resolve, reject) => {
+            try {
+                const res: sdk.Transaction[] = [];
+                const key_count = this.config.process.key_count;
+                let utxos: sdk.UnspentTxOutput[];
+                try {
+                    utxos = await this.boa_client.getUTXOs(WK.GenesisKey.address);
+                } catch (e) {
+                    logger.error(e);
+                    return reject(e);
+                }
+                if (utxos.length < 1) return resolve([]);
+
+                const count = utxos.length - 1;
+                const tx_out_count = Math.ceil(key_count / count);
+
+                for (let idx = 0; idx < count; idx++) {
+                    const tx_sz = sdk.Transaction.getEstimatedNumberOfBytes(1, tx_out_count, 0);
+                    const fees = await this.boa_client.getTransactionFee(tx_sz);
+                    const fee = sdk.JSBI.BigInt(fees.medium);
+
+                    const sum: sdk.JSBI = sdk.JSBI.subtract(utxos[idx].amount.value, fee);
+                    const amount = sdk.JSBI.divide(sum, sdk.JSBI.BigInt(tx_out_count));
+                    const remain = sdk.JSBI.subtract(sum, sdk.JSBI.multiply(amount, sdk.JSBI.BigInt(tx_out_count)));
+
+                    const builder = new sdk.TxBuilder(WK.GenesisKey);
+
+                    builder.addInput(utxos[idx].utxo, utxos[idx].amount);
+                    for (let key_idx = 0; key_idx < tx_out_count; key_idx++) {
+                        if (key_idx < tx_out_count - 1)
+                            builder.addOutput(WK.keys(idx * tx_out_count + key_idx).address, amount);
+                        else
+                            builder.addOutput(
+                                WK.keys(idx * tx_out_count + key_idx).address,
+                                sdk.JSBI.add(amount, remain)
+                            );
+                    }
+                    const tx = builder.sign(sdk.OutputType.Payment, fee);
+                    res.push(tx);
+                }
+
+                {
+                    const validators = [
+                        WK.NODE2().address,
+                        WK.NODE3().address,
+                        WK.NODE4().address,
+                        WK.NODE5().address,
+                        WK.NODE6().address,
+                        WK.NODE7().address,
+                        WK.NODE2().address,
+                        WK.NODE3().address,
+                        WK.NODE4().address,
+                        WK.NODE5().address,
+                        WK.NODE6().address,
+                        WK.NODE7().address,
+                        WK.NODE2().address,
+                        WK.NODE3().address,
+                        WK.NODE4().address,
+                        WK.NODE5().address,
+                        WK.NODE6().address,
+                        WK.NODE7().address,
+                        WK.NODE2().address,
+                        WK.NODE3().address,
+                        WK.NODE4().address,
+                        WK.NODE5().address,
+                        WK.NODE6().address,
+                        WK.NODE7().address,
+                        WK.NODE2().address,
+                        WK.NODE3().address,
+                        WK.NODE4().address,
+                        WK.NODE5().address,
+                        WK.NODE6().address,
+                        WK.NODE7().address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                        WK.keys(0).address,
+                    ];
+
+                    const tx_sz = sdk.Transaction.getEstimatedNumberOfBytes(1, validators.length, 0);
+                    const fees = await this.boa_client.getTransactionFee(tx_sz);
+                    const fee = sdk.JSBI.BigInt(fees.medium);
+
+                    const idx = utxos.length - 1;
+                    const sum: sdk.JSBI = sdk.JSBI.subtract(utxos[idx].amount.value, fee);
+                    const amount = sdk.JSBI.divide(sum, sdk.JSBI.BigInt(validators.length));
+                    const remain = sdk.JSBI.subtract(
+                        sum,
+                        sdk.JSBI.multiply(amount, sdk.JSBI.BigInt(validators.length))
+                    );
+                    const builder = new sdk.TxBuilder(WK.GenesisKey);
+                    builder.addInput(utxos[idx].utxo, utxos[idx].amount);
+                    for (let key_idx = 0; key_idx < validators.length; key_idx++) {
+                        if (key_idx < validators.length - 1) builder.addOutput(validators[key_idx], amount);
+                        else builder.addOutput(validators[key_idx], sdk.JSBI.add(amount, remain));
+                    }
+                    const tx = builder.sign(sdk.OutputType.Payment, fee);
+                    res.unshift(tx);
+                }
+
+                return resolve(res);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+
+    public sendDistributionTransaction(): Promise<any> {
+        return new Promise<any>(async (resolve, reject) => {
+            AutoRandomTxSender.doing_genesis = true;
+            try {
+                let send_count = 0;
+                let total_count = 0;
+                const hash: string[] = [];
+                const txs = await this.createTransaction();
+                total_count = txs.length;
+                if (txs.length > 0) {
+                    let tx_index = 0;
+                    for (const tx of txs) {
+                        const h = sdk.hashFull(tx).toString();
+                        hash.push(h);
+                        logger.info(
+                            `Send a transaction for distributing Genesis coins : (${tx_index + 1} / ${
+                                txs.length
+                            })  ${h}`
+                        );
+                        try {
+                            await this.boa_client.sendTransaction(tx);
+                            send_count++;
+                        } catch (e) {
+                            logger.error(e);
+                        }
+                        await wait(Config.getInstance().process.delay);
+                        tx_index++;
+                    }
+                }
+                AutoRandomTxSender.doing_genesis = false;
+                return resolve({
+                    status: true,
+                    data: {
+                        send_count,
+                        total_count,
+                        hash,
+                    },
+                });
+            } catch (e) {
+                AutoRandomTxSender.doing_genesis = false;
+                return resolve({
+                    status: false,
+                    error: e,
+                });
+            }
+        });
+    }
+
+    public send(): Promise<any> {
         return new Promise<any>((resolve) => {
             prepare().then(() => {
                 WK.make();
                 this.isDistributed()
                     .then((value) => {
                         if (value) {
-                            this.makeBlock().then((res) => {
-                                resolve(res);
-                            });
+                            if (!AutoRandomTxSender.doing_genesis) {
+                                logger.info("Send a random transaction");
+                                this.sendRandTransaction().then((res) => {
+                                    resolve(res);
+                                });
+                            } else {
+                                resolve({
+                                    status: false,
+                                    error: "Genesis coins are being distributed.",
+                                });
+                            }
                         } else {
-                            resolve({
-                                status: false,
-                                error: "Not distributed",
+                            logger.info("The distribution of Genesis coins started");
+                            this.sendDistributionTransaction().then((res) => {
+                                resolve(res);
                             });
                         }
                     })
--- src/modules/send/Distributor.ts
+++ src/modules/send/Distributor.ts
@@ -1,7 +1,7 @@
 import * as sdk from "boa-sdk-ts";
 
-import { logger, Logger } from "../common/Logger";
 import { Config } from "../common/Config";
+import { logger, Logger } from "../common/Logger";
 import { prepare, wait } from "../utils/Process";
 import { WK } from "../utils/WK";
 
@@ -17,12 +17,29 @@ export class Distributor {
         );
     }
 
-    createTransaction(height: sdk.JSBI): Promise<sdk.Transaction[]> {
+    public isDistributed(): Promise<boolean> {
+        return new Promise<any>(async (resolve, reject) => {
+            const wallet = new sdk.Wallet(WK.Genesis(), {
+                agoraEndpoint: this.config.server.agora_endpoint.toString(),
+                stoaEndpoint: this.config.server.stoa_endpoint.toString(),
+                fee: sdk.WalletFeeOption.Medium,
+            });
+            wallet.getBalance().then((res: sdk.IWalletResult) => {
+                if (res.code !== sdk.WalletResultCode.Success || res.data === undefined) return reject();
+
+                const balance: sdk.Balance = res.data;
+                if (sdk.JSBI.equal(balance.spendable, sdk.BOA(488_000_000).value)) return resolve(false);
+                else return resolve(true);
+            });
+        });
+    }
+
+    public createTransaction(): Promise<sdk.Transaction[]> {
         return new Promise<sdk.Transaction[]>(async (resolve, reject) => {
             try {
-                let res: sdk.Transaction[] = [];
-                let key_count = this.config.process.key_count;
-                let utxos: Array<sdk.UnspentTxOutput>;
+                const res: sdk.Transaction[] = [];
+                const key_count = this.config.process.key_count;
+                let utxos: sdk.UnspentTxOutput[];
                 try {
                     utxos = await this.boa_client.getUTXOs(WK.GenesisKey.address);
                 } catch (e) {
@@ -31,19 +48,19 @@ export class Distributor {
                 }
                 if (utxos.length < 1) return resolve([]);
 
-                let count = utxos.length - 1;
-                let tx_out_count = Math.ceil(key_count / count);
+                const count = utxos.length - 1;
+                const tx_out_count = Math.ceil(key_count / count);
 
                 for (let idx = 0; idx < count; idx++) {
-                    let tx_sz = sdk.Transaction.getEstimatedNumberOfBytes(1, tx_out_count, 0);
-                    let fees = await this.boa_client.getTransactionFee(tx_sz);
-                    let fee = sdk.JSBI.BigInt(fees.medium);
+                    const tx_sz = sdk.Transaction.getEstimatedNumberOfBytes(1, tx_out_count, 0);
+                    const fees = await this.boa_client.getTransactionFee(tx_sz);
+                    const fee = sdk.JSBI.BigInt(fees.medium);
 
-                    let sum: sdk.JSBI = sdk.JSBI.subtract(utxos[idx].amount.value, fee);
-                    let amount = sdk.JSBI.divide(sum, sdk.JSBI.BigInt(tx_out_count));
-                    let remain = sdk.JSBI.subtract(sum, sdk.JSBI.multiply(amount, sdk.JSBI.BigInt(tx_out_count)));
+                    const sum: sdk.JSBI = sdk.JSBI.subtract(utxos[idx].amount.value, fee);
+                    const amount = sdk.JSBI.divide(sum, sdk.JSBI.BigInt(tx_out_count));
+                    const remain = sdk.JSBI.subtract(sum, sdk.JSBI.multiply(amount, sdk.JSBI.BigInt(tx_out_count)));
 
-                    let builder = new sdk.TxBuilder(WK.GenesisKey);
+                    const builder = new sdk.TxBuilder(WK.GenesisKey);
 
                     builder.addInput(utxos[idx].utxo, utxos[idx].amount);
                     for (let key_idx = 0; key_idx < tx_out_count; key_idx++) {
@@ -55,12 +72,12 @@ export class Distributor {
                                 sdk.JSBI.add(amount, remain)
                             );
                     }
-                    let tx = builder.sign(sdk.OutputType.Payment, fee);
+                    const tx = builder.sign(sdk.OutputType.Payment, fee);
                     res.push(tx);
                 }
 
                 {
-                    let validators = [
+                    const validators = [
                         WK.NODE2().address,
                         WK.NODE3().address,
                         WK.NODE4().address,
@@ -147,21 +164,24 @@ export class Distributor {
                         WK.keys(0).address,
                     ];
 
-                    let tx_sz = sdk.Transaction.getEstimatedNumberOfBytes(1, validators.length, 0);
-                    let fees = await this.boa_client.getTransactionFee(tx_sz);
-                    let fee = sdk.JSBI.BigInt(fees.medium);
+                    const tx_sz = sdk.Transaction.getEstimatedNumberOfBytes(1, validators.length, 0);
+                    const fees = await this.boa_client.getTransactionFee(tx_sz);
+                    const fee = sdk.JSBI.BigInt(fees.medium);
 
-                    let idx = utxos.length - 1;
-                    let sum: sdk.JSBI = sdk.JSBI.subtract(utxos[idx].amount.value, fee);
-                    let amount = sdk.JSBI.divide(sum, sdk.JSBI.BigInt(validators.length));
-                    let remain = sdk.JSBI.subtract(sum, sdk.JSBI.multiply(amount, sdk.JSBI.BigInt(validators.length)));
-                    let builder = new sdk.TxBuilder(WK.GenesisKey);
+                    const idx = utxos.length - 1;
+                    const sum: sdk.JSBI = sdk.JSBI.subtract(utxos[idx].amount.value, fee);
+                    const amount = sdk.JSBI.divide(sum, sdk.JSBI.BigInt(validators.length));
+                    const remain = sdk.JSBI.subtract(
+                        sum,
+                        sdk.JSBI.multiply(amount, sdk.JSBI.BigInt(validators.length))
+                    );
+                    const builder = new sdk.TxBuilder(WK.GenesisKey);
                     builder.addInput(utxos[idx].utxo, utxos[idx].amount);
                     for (let key_idx = 0; key_idx < validators.length; key_idx++) {
                         if (key_idx < validators.length - 1) builder.addOutput(validators[key_idx], amount);
                         else builder.addOutput(validators[key_idx], sdk.JSBI.add(amount, remain));
                     }
-                    let tx = builder.sign(sdk.OutputType.Payment, fee);
+                    const tx = builder.sign(sdk.OutputType.Payment, fee);
                     res.unshift(tx);
                 }
 
@@ -172,31 +192,30 @@ export class Distributor {
         });
     }
 
-    makeBlock(): Promise<any> {
+    public makeBlock(): Promise<any> {
         return new Promise<any>(async (resolve, reject) => {
             try {
-                let height: sdk.JSBI = sdk.JSBI.BigInt(0);
+                let distributed: boolean;
                 try {
-                    height = await this.boa_client.getBlockHeight();
+                    distributed = await this.isDistributed();
                 } catch (e) {
-                    logger.error(e);
                     return resolve({
                         status: false,
                         error: e,
                     });
                 }
 
-                if (sdk.JSBI.lessThan(height, sdk.JSBI.BigInt(30))) {
+                if (!distributed) {
                     let send_count = 0;
                     let total_count = 0;
-                    let hash: Array<string> = [];
-                    let txs = await this.createTransaction(height);
+                    const hash: string[] = [];
+                    const txs = await this.createTransaction();
                     total_count = txs.length;
                     if (txs.length > 0) {
-                        for (let tx of txs) {
-                            let h = sdk.hashFull(tx).toString();
+                        for (const tx of txs) {
+                            const h = sdk.hashFull(tx).toString();
                             hash.push(h);
-                            logger.info(`TX_HASH (send / ${height.toString()}) : ${h}`);
+                            logger.info(`TX_HASH (send ) : ${h}`);
                             try {
                                 await this.boa_client.sendTransaction(tx);
                                 send_count++;
@@ -209,9 +228,9 @@ export class Distributor {
                     return resolve({
                         status: true,
                         data: {
-                            send_count: send_count,
-                            total_count: total_count,
-                            hash: hash,
+                            send_count,
+                            total_count,
+                            hash,
                         },
                     });
                 } else {
@@ -229,11 +248,11 @@ export class Distributor {
         });
     }
 
-    send(): Promise<any> {
+    public send(): Promise<any> {
         return new Promise<any>(async (resolve) => {
             await prepare();
             WK.make();
-            let res = await this.makeBlock();
+            const res = await this.makeBlock();
             resolve(res);
         });
     }
